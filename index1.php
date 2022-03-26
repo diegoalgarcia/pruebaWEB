a39430478256a38d1ca82c4c673254787f4624ec
diegoalgarcia <dagarciaag@ut.edu.co>
2022-03-26 16:43:47 -0500

    mi primer commit

---
+++ index1.php
@@ -0,0 +1,41 @@
+<?php 
+  require_once ("ClassOperacion.php");
+
+  $operacion1 = new Operacion(10, 2);
+  $operacion2 = new Operacion(100, 3);
+
+  $totalSuma = $operacion1->getSuma();
+  echo "Total Suma: ".$totalSuma;
+  echo "<br>";
+
+  $totalSuma = $operacion1->getResta();
+  echo "Total Resta: ".$totalSuma;
+  echo "<br>";
+
+  $totalSuma = $operacion1->getMultiplicacion();
+  echo "Total Multiplicación: ".$totalSuma;
+  echo "<br>";
+
+  $totalSuma = $operacion1->getDivision();
+  echo "Total División: ".$totalSuma;
+  echo "<br>";
+
+  echo "=================================================== <br>";
+
+
+  $totalSuma = $operacion2->getSuma();
+  echo "Total Suma: ".$totalSuma;
+  echo "<br>";
+
+  $totalSuma = $operacion2->getResta();
+  echo "Total Resta: ".$totalSuma;
+  echo "<br>";
+
+  $totalSuma = $operacion2->getMultiplicacion();
+  echo "Total Multiplicación: ".$totalSuma;
+  echo "<br>";
+
+  $totalSuma = $operacion2->getDivision();
+  echo "Total División: ".$totalSuma;
+  echo "<br>";
+?>
